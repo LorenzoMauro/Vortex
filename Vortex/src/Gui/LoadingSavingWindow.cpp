f57822e447d7ad33756c2f38ed5d29e624f78b0d
Lorenzo Mauro <lorenzo.lomar.mauro@gmail.com>
2023-09-19 13:37:35 +0200

    Improved Loading Saving Dialog

--- Vortex/src/Gui/LoadingSavingWindow.cpp
+++ Vortex/src/Gui/LoadingSavingWindow.cpp
@@ -7,8 +7,8 @@ namespace vtx
 	void savingLoadingPreRender()
 	{
 		const ImGuiIO& io         = ImGui::GetIO();
-		const auto     windowSize = ImVec2(io.DisplaySize.x * 0.10f, io.DisplaySize.y * 0.10f);
-		const auto     windowPos  = ImVec2((io.DisplaySize.x - windowSize.x) * 0.5f, (io.DisplaySize.y - windowSize.y) * 0.5f);
+		const auto     windowSize = ImVec2(io.DisplaySize.x * 0.20f, io.DisplaySize.y * 0.10f);
+		const auto     windowPos  = ImVec2((io.DisplaySize.x - windowSize.x) * 0.5f, (io.DisplaySize.y - windowSize.y) * 0.25f);
 
 
 		ImGui::SetNextWindowPos(windowPos);
@@ -16,7 +16,7 @@ namespace vtx
 	}
 	LoadingWindow::LoadingWindow()
 	{
-		windowFlags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoDocking;
+		windowFlags |= ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoDocking;
 		useToolbar = false;
 	}
 
@@ -36,12 +36,13 @@ namespace vtx
 	void LoadingWindow::renderMainContent()
 	{
 		const std::string filePathToLoad = LoadingSaving::get().getFilePathToLoad();
-		ImGui::Text("Loading file: %s\n This might take a while, you can check the terminal for more info", filePathToLoad.c_str());
+		ImGui::TextWrapped("Loading file: \n%s", filePathToLoad.c_str());
+		ImGui::TextWrapped("Hold tight, it might take a while as data is being loaded on Gpu!\nYou can check the terminal for more info");
 	}
 
 	SavingWindow::SavingWindow()
 	{
-		windowFlags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoDocking;
+		windowFlags |= ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoDocking;
 		useToolbar = false;
 	}
 
@@ -61,6 +62,6 @@ namespace vtx
 	void SavingWindow::renderMainContent()
 	{
 		const std::string filePathToSave = LoadingSaving::get().getFilePathToSave();
-		ImGui::Text("Saving file: %s\n", filePathToSave.c_str());
+		ImGui::TextWrapped("Saving file:\n%s", filePathToSave.c_str());
 	}
 }
